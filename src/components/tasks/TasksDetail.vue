59a40468c2d4411700b9282fa3d42850c08e82e5
moha5145 <mms.jimma@gmail.com>
2021-05-25 17:43:56 +0200

    Initial repo

--- src/components/tasks/TasksDetail.vue
+++ src/components/tasks/TasksDetail.vue
@@ -1,72 +1,74 @@
 <template>
-    <div v-if="category[0]"  class=" q-mx-sm" style="border-radius: 20px" >
-      <q-card  flat bordered style="border-radius: 20px" class="q-pt-none q-mt-md">
-      
-        <div class="row">
-          <q-btn flat round to="/" icon="arrow_back" class="q-mt-sm q-ml-sm"
-            :style="{'color': store.state.themeColor}">
-          </q-btn>
+    <div   v-if="category[0]"  class="row q-mx-sm justify-center" style="border-radius: 20px" >
+      <div class="col-12 col-sm-10 col-md-8">
+        <q-card  flat bordered style="border-radius: 20px" class="q-pt-none q-mt-md ">
+        
+          <div class="row">
+            <q-btn flat round to="/" icon="arrow_back" class="q-mt-sm q-ml-sm"
+              :style="{'color': store.state.themeColor}">
+            </q-btn>
 
-          <q-space />
-          
-          <!-- <div style="padding-top: 5px;" class="q-mr-xs">
-            <q-fab
-              v-model="fab"
-              size="20px"
-              dense                
-              icon="keyboard_arrow_right"
-              direction="left"
-              padding="xs"
-              :style="{'color': store.state.themeColor}"
-              class="q-mt-sm q-mr-sm"
-            >
-              
-              <DeleteCategory :category="category[0]" />
-              
-              <EditCategory :category="category[0]"/>
-              
-            </q-fab>
-          </div> -->
-        </div>
+            <q-space />
+            
+            <!-- <div style="padding-top: 5px;" class="q-mr-xs">
+              <q-fab
+                v-model="fab"
+                size="20px"
+                dense                
+                icon="keyboard_arrow_right"
+                direction="left"
+                padding="xs"
+                :style="{'color': store.state.themeColor}"
+                class="q-mt-sm q-mr-sm"
+              >
+                
+                <DeleteCategory :category="category[0]" />
+                
+                <EditCategory :category="category[0]"/>
+                
+              </q-fab>
+            </div> -->
+          </div>
 
-        <h6  class="text-center q-my-sm text-grey-8">{{category_slug.toUpperCase()}}</h6 >
-          <AddAndEditForm 
-            :category="category[0]"
-            :task="store.state.task"
-            :submitForm="store.methods.addTask">
-          </AddAndEditForm>
+          <h6  class="text-center q-my-sm text-grey-8">{{category_slug.toUpperCase()}}</h6 >
+            <AddAndEditForm 
+              :category="category[0]"
+              :task="store.state.task"
+              :submitForm="store.methods.addTask">
+            </AddAndEditForm>
 
-        <q-tabs v-show="displayTab"
-            v-model="tab"
-            dense
-            class=""
-            dark
-            active-color=orange
-            indicator-color=orange
-            align="justify"
-        >
-          <q-tab name="all" :label="$t('all')" />
-          <q-tab name="ongoing" :label="$t('ongoing')" />
-          <q-tab name="completed" :label="$t('completed')" />
-        </q-tabs>
+          <q-tabs v-show="displayTab"
+              v-model="tab"
+              dense
+              class=""
+              dark
+              active-color=orange
+              indicator-color=orange
+              align="justify"
+          >
+            <q-tab name="all" :label="$t('all')" />
+            <q-tab name="ongoing" :label="$t('ongoing')" />
+            <q-tab name="completed" :label="$t('completed')" />
+          </q-tabs>
 
-        <!-- <q-separator /> -->
+          <!-- <q-separator /> -->
 
-        <q-tab-panels v-model="tab" animated class=" q-px-none  q-mb-none">
-          <q-tab-panel name="all" class="q-px-sm q-pb-xs" >
-            <AllTasks :todoList="store.state.todoList" :category="category[0].slug"/>
-          </q-tab-panel>
+          <q-tab-panels v-model="tab" animated class=" q-px-none  q-mb-none">
+            <q-tab-panel name="all" class="q-px-sm q-pb-xs" >
+              <AllTasks :todoList="store.state.todoList" :category="category[0].slug"/>
+            </q-tab-panel>
 
-          <q-tab-panel name="ongoing" class="q-px-sm q-pb-xs" >
-            <TodoOnGoing :todoList="store.state.todoList" :category="category[0].slug"></TodoOnGoing>
-          </q-tab-panel>
+            <q-tab-panel name="ongoing" class="q-px-sm q-pb-xs" >
+              <TodoOnGoing :todoList="store.state.todoList" :category="category[0].slug"></TodoOnGoing>
+            </q-tab-panel>
 
-          <q-tab-panel name="completed" class="q-px-sm q-pb-xs" >
-            <TodoCompleted :todoList="store.state.todoList" :category="category[0].slug"></TodoCompleted>
-          </q-tab-panel>
+            <q-tab-panel name="completed" class="q-px-sm q-pb-xs" >
+              <TodoCompleted :todoList="store.state.todoList" :category="category[0].slug"></TodoCompleted>
+            </q-tab-panel>
 
-        </q-tab-panels>
-      </q-card>
+          </q-tab-panels>
+        </q-card>
+      </div>
     </div>
 </template>
 
